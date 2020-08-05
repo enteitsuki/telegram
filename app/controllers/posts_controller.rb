f7b80921e85aa085abbc3cc61af44659b17802e2
enteitsuki <enteitsuki@gmail.com>
2020-08-06 00:42:06 +0900

    検索機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,9 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :edit, :update, :destroy]
 
   def index
-    @posts = Post.find_newest_post(params[:page]).with_user_and_comment
-    @comment = Comment.new
+      @q = Post.ransack(params[:q])
+      @posts = @q.result.find_newest_post(params[:page]).with_user_and_comment
+      @comment = Comment.new
   end
 
   def new
