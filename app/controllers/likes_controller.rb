0f214b0ca3d5e2d3f0011c1e880a720cc7024f28
enteitsuki <enteitsuki@gmail.com>
2020-08-06 02:14:23 +0900

    Ajaxによる「いいね」の非同期処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
   def like
     like = Like.new(post_id: params[:post_id], user_id: current_user.id)
     like.save
-    redirect_to posts_path
   end
 
   def unlike
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
-    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
 end
